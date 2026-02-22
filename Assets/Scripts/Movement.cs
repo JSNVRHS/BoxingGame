3b46c45d264dff787b72f3809fbb067b93d45fff
ATY ESHMBKOV <atayeshimbekov@gmail.com>
2026-02-22 23:56:53 +0100

    Movement + borders
    1.Borders of the ring with collision
    2.Movement of the player character

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,15 +2,70 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+
+
+    public Rigidbody2D body;
+    public Animator anim;
+    public bool isMoving = false;
+    public float speed = 2f;
+
+    public float x;
+    public float y;
+
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        x = 0.0f;
+        y = 0.0f;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        x = 0.0f;
+        y = 0.0f;
+
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            y += 1;
+            isMoving = true;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            x -= 1;
+            isMoving = true;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            x += 1;
+            isMoving = true;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            y -= 1;
+            isMoving = true;
+        }
+
+       
+
+        Vector2 direction = new Vector2(x, y).normalized;;
+
+        anim.SetBool("walking", isMoving);
         
+        body.linearVelocity = direction * speed;
+
+        if (isMoving)
+        {
+            isMoving = false;
+        }
+
+
+
+
+
     }
 }
