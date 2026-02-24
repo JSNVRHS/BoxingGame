327689312877201a4a224fc410b206b47212e59d
ATY ESHMBKOV <atayeshimbekov@gmail.com>
2026-02-24 02:39:21 +0100

    TORSO + LEGS
    1. Torso
     a) rotation towards mouse
     b) stick to legs
     c) ducking
     d) sprites
    
    2. Legs
     a) movement
     b) rotation + direction
     c) animations + sprites

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -11,8 +11,9 @@ public class Movement : MonoBehaviour
 
     public float x;
     public float y;
+    public float z;
 
-    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,6 +22,7 @@ public class Movement : MonoBehaviour
 
         x = 0.0f;
         y = 0.0f;
+        z = 0.0f;
 
     }
 
@@ -30,33 +32,39 @@ public class Movement : MonoBehaviour
         x = 0.0f;
         y = 0.0f;
 
-        if (Input.GetKey(KeyCode.UpArrow)) {
+        if (Input.GetKey(KeyCode.W)) {
             y += 1;
+            z = 0;
             isMoving = true;
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.A))
         {
             x -= 1;
+            z = 90;
             isMoving = true;
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.D))
         {
             x += 1;
+            z = -90;
             isMoving = true;
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.S))
         {
             y -= 1;
+            z = 180;
             isMoving = true;
         }
 
        
-
+        // setting up the rotation of the legs and their direction for moving
         Vector2 direction = new Vector2(x, y).normalized;;
-
+        Quaternion turn = Quaternion.Euler(0, 0, z);
+        // set property true/false for animation change
         anim.SetBool("walking", isMoving);
-        
+        // applying new rotation and direction
         body.linearVelocity = direction * speed;
+        body.transform.rotation = Quaternion.Slerp(body.transform.rotation, turn, 1);
 
         if (isMoving)
         {
---
+++ Assets/Scripts/Torso.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class Torso : MonoBehaviour
+{
+
+
+    public Rigidbody2D torso;
+    public Animator anim;
+    public GameObject legs;
+
+    public Camera cam;
+
+    public bool duck = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        torso = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        cam = Camera.main;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotateAtMouse();
+        Duck();
+        stickToLegs();
+    }
+
+
+
+    public void stickToLegs()
+    {
+        torso.transform.position = legs.transform.position;
+        torso.transform.rotation = torso.transform.rotation;
+    }
+
+    
+
+
+
+
+
+    public void rotateAtMouse()
+    {
+        Vector3 mousePos = (Vector2)cam.ScreenToWorldPoint(Input.mousePosition);
+        float angleRad = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x);
+        float angleDeg = (180/Mathf.PI)*angleRad - 90;
+
+        torso.transform.rotation = Quaternion.Euler(0f, 0f, angleDeg);
+    }
+
+
+    public void Duck() {
+        
+            if (Input.GetKey(KeyCode.Space)) {
+                    duck = true;
+            }
+            else
+            {
+                duck = false;
+            }
+            
+
+        anim.SetBool("duck", duck);
+
+        
+    
+    }
+}
