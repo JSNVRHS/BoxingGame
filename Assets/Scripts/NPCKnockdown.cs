8331a6a2511c46ae682f4f72d7af929ba5ff7cca
ATY ESHMBKOV <atayeshimbekov@gmail.com>
2026-04-20 06:01:04 +0200

    Major
    1. Changed the hierarchy of the torso (for duck mode)
    
    2. Merged features of Head.cs and testHead.cs into testHead.cs
    
    3. Added sprites for knocked down version of player character

--- Assets/Scripts/NPCKnockdown.cs
+++ Assets/Scripts/NPCKnockdown.cs
@@ -6,9 +6,22 @@ public class NPCKnockdown : MonoBehaviour
     public GameObject legs;
     public GameObject headPrefab;
     public GameObject knockdownSprite;
+    [SerializeField] float rotationOffset = 0f;
 
     public void Knockdown()
     {
+        if (knockdownSprite != null)
+        {
+            knockdownSprite.transform.position = headPrefab != null
+                ? headPrefab.transform.position
+                : transform.position;
+            Quaternion sourceRotation = headPrefab != null
+                ? headPrefab.transform.rotation
+                : transform.rotation;
+            knockdownSprite.transform.rotation =
+                sourceRotation * Quaternion.Euler(0f, 0f, rotationOffset);
+        }
+
         torso.SetActive(false);
         legs.SetActive(false);
         headPrefab.SetActive(false);
---
+++ Assets/Scripts/testHead.cs
@@ -0,0 +1,183 @@
+using UnityEngine;
+
+public class testHead : MonoBehaviour
+{
+    public Rigidbody2D head;
+    public GameObject torso;
+    public Camera cam;
+
+    [Header("Mode")]
+    [SerializeField] bool aimAtMouse = false;
+    [SerializeField] float aimAngleOffset = 0f;
+
+    [Header("Ownership")]
+    [SerializeField] Transform ownerRoot;
+    [SerializeField] Transform opponentRoot;
+
+    [Header("Effects / Knockdown")]
+    [SerializeField] GameObject bloodPrefab;
+    [SerializeField] Transform chinTransform;
+    [SerializeField] RingFlash ringFlash;
+    [SerializeField] NPCKnockdown knockdown;
+
+    [Header("Hit Reaction")]
+    [SerializeField] float baseAngleOffset = 40f;
+    [SerializeField] float hitRotationAmount = 30f;
+    [SerializeField] float returnSpeed = 2f;
+    [SerializeField] float detectionRadius = 0.5f;
+    [SerializeField] float hitCooldown = 0.5f;
+    [SerializeField] int hitsToKnockdown = 4;
+
+    float lockedHeadAngle = 0f;
+    float reactionAngleOffset = 0f;
+
+    int hitCount = 0;
+    bool knockedDown = false;
+    bool recentlyHit = false;
+
+    void Start()
+    {
+        head = GetComponent<Rigidbody2D>();
+
+        if (aimAtMouse)
+        {
+            cam = Camera.main;
+        }
+    }
+
+    void Update()
+    {
+        if (knockedDown) return;
+
+        StickToTorso();
+        reactionAngleOffset = Mathf.LerpAngle(
+            reactionAngleOffset,
+            0f,
+            Time.deltaTime * returnSpeed
+        );
+
+        if (aimAtMouse)
+        {
+            RotateAtMouse();
+        }
+        else
+        {
+            UpdateReactionRotation();
+        }
+
+        if (!recentlyHit)
+        {
+            CheckForHits();
+        }
+    }
+
+    void StickToTorso()
+    {
+        head.transform.position = torso.transform.position;
+    }
+
+    void RotateAtMouse()
+    {
+        if (cam == null) return;
+
+        Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f;
+
+        float angleRad = Mathf.Atan2(
+            mousePos.y - transform.position.y,
+            mousePos.x - transform.position.x
+        );
+        float baseAngle = Mathf.Rad2Deg * angleRad - 90f + aimAngleOffset;
+
+        if (RightHand.isPunching || LeftHand.isPunching)
+        {
+            head.transform.rotation = Quaternion.Euler(0f, 0f, lockedHeadAngle + reactionAngleOffset);
+        }
+        else
+        {
+            lockedHeadAngle = baseAngle;
+            head.transform.rotation = Quaternion.Euler(0f, 0f, baseAngle + reactionAngleOffset);
+        }
+    }
+
+    void UpdateReactionRotation()
+    {
+        head.transform.rotation =
+            torso.transform.rotation * Quaternion.Euler(0f, 0f, baseAngleOffset + reactionAngleOffset);
+    }
+
+    void CheckForHits()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, detectionRadius);
+
+        foreach (Collider2D hit in hits)
+        {
+            if (ownerRoot != null && (hit.transform == ownerRoot || hit.transform.IsChildOf(ownerRoot)))
+                continue;
+
+            if (opponentRoot != null && hit.transform != opponentRoot && !hit.transform.IsChildOf(opponentRoot))
+                continue;
+
+            if (hit.CompareTag("RightHand"))
+            {
+                RegisterHit(true);
+                return;
+            }
+
+            if (hit.CompareTag("LeftHand"))
+            {
+                RegisterHit(false);
+                return;
+            }
+        }
+    }
+
+    void RegisterHit(bool isRightHand)
+    {
+        hitCount++;
+        recentlyHit = true;
+        Invoke(nameof(ResetHit), hitCooldown);
+
+        if (hitCount >= hitsToKnockdown)
+        {
+            knockedDown = true;
+
+            if (ringFlash != null)
+                ringFlash.StayHit();
+
+            if (knockdown != null)
+                knockdown.Knockdown();
+
+            return;
+        }
+
+        if (isRightHand)
+            reactionAngleOffset = -hitRotationAmount;
+        else
+            reactionAngleOffset = hitRotationAmount;
+
+        SpawnBlood();
+
+        if (ringFlash != null)
+            ringFlash.Flash();
+    }
+
+    void ResetHit()
+    {
+        recentlyHit = false;
+    }
+
+    void SpawnBlood()
+    {
+        if (bloodPrefab != null && chinTransform != null)
+        {
+            Instantiate(bloodPrefab, chinTransform.position, chinTransform.rotation);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
+    }
+}
